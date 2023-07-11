4c5491fb04dd7be6ea8dbcacfbff8fba6c688363
bjornunnar <bjornunnar@gmail.com>
2023-07-11 11:44:38 +0000

    fonts

--- slick-init.js
+++ slick-init.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     setTimeout(function(){
         $('.single-item').slick({
-            adaptiveHeight: true,
             autoplay: true,
             autoplaySpeed: 10000,
             arrows: false,
