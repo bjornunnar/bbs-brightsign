787a45b4b558ef138c82109f346b1135e034908e
bjornunnar <bjornunnar@gmail.com>
2023-07-14 12:15:59 +0000

    containers and margins

--- events-slideshow/bbs-fetch-parse-rss.js
+++ events-slideshow/bbs-fetch-parse-rss.js
@@ -18,7 +18,7 @@ $.ajax(RSS_URL, {
         const template = `
           <div class="item">
             <h1 class="title">${el.find("title").text()}</h1>
-            <img src="${el.find("media\\:content").attr("url")}" alt="">
+            <div class="image-container"><img class="title-image" src="${el.find("media\\:content").attr("url")}" alt=""></div>
             <div class="description">${el.find("description").text()}</div>
             <div class="starttime"><img class="calendar icon" src="calendar.png">${el.find("content-rss\\:arrangement-starttime").text()}</div>
             <div class="location"><img class="location icon" src="location.png">${el.find("content-rss\\:arrangement-location").text()}</div>
