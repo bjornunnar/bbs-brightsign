bcc8954ebf8216e677096b9c493f2db136b9b4d3
bjornunnar <bjornunnar@gmail.com>
2023-07-14 15:05:10 +0000

    Title, portait-oriented image support, bg fade

--- events-slideshow/bbs-fetch-parse-rss.js
+++ events-slideshow/bbs-fetch-parse-rss.js
@@ -17,8 +17,8 @@ $.ajax(RSS_URL, {
 
         const template = `
           <div class="item">
-            <h1 class="title">${el.find("title").text()}</h1>
-            <div class="image-container"><img class="title-image" src="${el.find("media\\:content").attr("url")}" alt=""></div>
+            <h2 class="event-title">${el.find("title").text()}</h1>
+            <div class="image-container" style="background-image: url('${el.find("media\\:content").attr("url")}'); background-size: cover;"><img class="title-image" src='${el.find("media\\:content").attr("url")}' alt=""></div>
             <div class="description">${el.find("description").text()}</div>
             <div class="starttime"><img class="calendar icon" src="calendar.png">${el.find("content-rss\\:arrangement-starttime").text()}</div>
             <div class="location"><img class="location icon" src="location.png">${el.find("content-rss\\:arrangement-location").text()}</div>
@@ -26,6 +26,13 @@ $.ajax(RSS_URL, {
         `;
 
         slideShowDiv[0].insertAdjacentHTML("beforeend", template);
+
+        /* add portrait class to portrait-oriented images */
+        $('img').each(function() {
+          if ($(this).width() <= $(this).height()) {
+            $(this).addClass('portrait');
+          }
+        })
       });
   }
 });
