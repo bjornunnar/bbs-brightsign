0749dcf50013ba31b4b4ccf3ff03fd790b6d044c
bjornunnar <bjornunnar@gmail.com>
2023-07-14 15:42:38 +0000

    opaque background to image container

--- events-slideshow/bbs-fetch-parse-rss.js
+++ events-slideshow/bbs-fetch-parse-rss.js
@@ -18,7 +18,7 @@ $.ajax(RSS_URL, {
         const template = `
           <div class="item">
             <h2 class="event-title">${el.find("title").text()}</h1>
-            <div class="image-container" style="background-image: url('${el.find("media\\:content").attr("url")}'); background-size: cover;"><img class="title-image" src='${el.find("media\\:content").attr("url")}' alt=""></div>
+            <div class="image-container" style="background-image: linear-gradient(to bottom, rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.7)), url('${el.find("media\\:content").attr("url")}'); background-image: -webkit-linear-gradient(top, rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.7)), url('${el.find("media\\:content").attr("url")}'); background-size: cover;"><img class="title-image" src='${el.find("media\\:content").attr("url")}' alt=""></div>
             <div class="description">${el.find("description").text()}</div>
             <div class="starttime"><img class="calendar icon" src="calendar.png">${el.find("content-rss\\:arrangement-starttime").text()}</div>
             <div class="location"><img class="location icon" src="location.png">${el.find("content-rss\\:arrangement-location").text()}</div>
