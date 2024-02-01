e6d5ab598012aca22ded337796056588c7fdc93f
bjornunnar <bjornunnar@gmail.com>
2024-02-01 16:13:15 +0000

    tweak fonts and icons

--- events-slideshow/bbs-fetch-parse-rss.js
+++ events-slideshow/bbs-fetch-parse-rss.js
@@ -35,11 +35,12 @@ $.ajax(RSS_URL, {
             <h2 class="event-title">${el.find("title").text()}</h1>
             <div class="image-container" style="background-image: linear-gradient(to bottom, rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.7)), url('${titleImageURL}'); background-image: -webkit-linear-gradient(top, rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.7)), url('${titleImageURL}'); background-size: cover;"><img class="title-image" src='${titleImageURL}' alt=""></div>
             <div class="description">${el.find("description").text()}</div>
-            <div class="category">${el.find("content-rss\\:organizers").text()}</div>
+            
             <div class="time-and-place">
-              <div class="starttime"><img class="calendar icon" src="calendar.png">${startTime}</div>
-              <div class="location"><img class="location icon" src="location.png">${el.find("content-rss\\:arrangement-location").text()}</div>
+              <div class="starttime"><img class="calendar icon" src="calendar_large.png">${startTime}</div>
+              <div class="location"><img class="location icon" src="location_large.png">${el.find("content-rss\\:arrangement-location").text()}</div>
             </div>
+            <div class="category"><img class="category icon" src="category.png">${el.find("content-rss\\:organizers").text()}</div>
           </div>
         `;
 
