a4661aa691f605a2dbb2fe1aa4ff25e365805917
Björn Unnar <35260726+bjornunnar@users.noreply.github.com>
2024-09-04 12:02:04 +0000

    add empty/error message, restructure

--- events-manual/app.js
+++ events-manual/app.js
@@ -4,12 +4,17 @@ const AIRTABLE_ACCESS_TOKEN = ''; // <-- add the airtable access token
 const BASE_ID = 'appdvXMtHepP6jUbx';
 const TABLE_NAME = 'tblS6VqaruyLM6O2M'; // grofin
 const VIEW_NAME = 'timeline'; // this view is set up to drop past events from the output
+const PAGE_TITLE = 'Grófin í dag';
+let singleMessage = 'Ekkert á döfinni í dag.';
 
-function buildTitleWithDate(){
-    const currentDate = new Date().toISOString().slice(0, 10)
+function buildTitleWithDate(pageTitle){
+    const currentDate = new Date();
+    const dateOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+    const dateString = currentDate.toLocaleDateString("is-IS", dateOptions);
     const title = document.createElement("h1");
-    title.innerHTML = "Grófin í dag" + " " + currentDate;
+    title.innerHTML = pageTitle + " " + dateString;
     const titleElement = document.getElementById('title');
+    titleElement.innerHTML = '';
 
     titleElement.appendChild(title);
 }
@@ -23,32 +28,40 @@ async function fetchRecords() {
         });
 
         if (!response.ok) {
+            
             throw new Error('Failed to fetch data from Airtable');
         }
 
         const data = await response.json();
-        displayRecords(data.records);
+
+        return data.records || [];
+        
     } catch (error) {
-        console.error('Error fetching records:', error);
+        singleMessage = 'Næ ekki sambandi við dagskrá';
+        return [];
     }
 }
 
-function displayRecords(records) {
-    const recordList = document.getElementById('agenda');
-    recordList.innerHTML = ''; // Clear previous records
+function displayMessage(messageSpace, message) {
+    messageSpace.innerHTML = '<div class="message">' + message + '</div>';
+
+}
+
+function displayRecords(records, recordSpace) {
+    console.log(records);
 
     // Separate records based on the "always on top" category
     const alwaysOnTopRecords = records.filter(record => record.fields.Status === 'always on top');
     const otherRecords = records.filter(record => record.fields.Status !== 'always on top');
 
     // Display "always on top" records first
-    alwaysOnTopRecords.forEach(record => createAndInsertRecord(record, recordList, true));
+    alwaysOnTopRecords.forEach(record => createAndInsertRecord(record, recordSpace, true));
 
     // Display other records in their original order
-    otherRecords.forEach(record => createAndInsertRecord(record, recordList, false));
+    otherRecords.forEach(record => createAndInsertRecord(record, recordSpace, false));
 }
 
-function createAndInsertRecord(record, recordList, insertAtTop) {
+function createAndInsertRecord(record, recordSpace, insertAtTop) {
 
         const { Title, Location, "Start time": startTime, "End time": endTime, Status, Notes } = record.fields;
         const timeOptions = { hour: "2-digit", minute: "2-digit", hour12: false };
@@ -71,17 +84,39 @@ function createAndInsertRecord(record, recordList, insertAtTop) {
         
         // Insert the list item at the top or bottom based on the category
     if (insertAtTop) {
-        recordList.prepend(listItem);
+        recordSpace.prepend(listItem);
     } else {
-        recordList.appendChild(listItem);
+        recordSpace.appendChild(listItem);
     }
 }
 
+async function buildContent() {
+    const recordSpace = document.getElementById('agenda');
+    recordSpace.innerHTML = ''; // Clear previous records
+    const messageSpace = document.getElementById('single-message');
+    messageSpace.innerHTML = ''; // Clear previous message
+
+
+        // Initial fetch
+    const records = await fetchRecords();
+
+    if (records.length === 0){
+        displayMessage(messageSpace, singleMessage);
+    } else {
+        // display records
+    displayRecords(records, recordSpace);
+    }
+
+    
+}
 // Add title
-buildTitleWithDate();
+buildTitleWithDate(PAGE_TITLE);
 
-// Initial fetch
-fetchRecords();
+// build content
+buildContent();
 
-// Fetch records every 5 minutes (300000 milliseconds)
-setInterval(fetchRecords, 300000);
+// rebuild title every hour
+setInterval(buildTitleWithDate(PAGE_TITLE), 6000000);
+
+// Rebuild content every 5 minutes (300000 milliseconds)
+setInterval(buildContent, 300000);
